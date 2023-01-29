cc132ced9721d63b42f05000b03645d7e92f78fb
napund4 <napunda@mail.com>
2023-01-29 19:09:57 -0300

    sync

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 require("dotenv").config();
 require("./config/db");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const router = require("./src/routes/router");
 
 app.use("/", router);
